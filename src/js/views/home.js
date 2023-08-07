4fb1bbc75ac0b38964b44eba1147d87d0c056925
Juan Ramos <127767142+jars4u@users.noreply.github.com>
2023-08-07 23:07:11 +0000

    contactCard component created

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -1,11 +1,30 @@
-import React, {useEffect, useState, useContext} from "react";
-import {Link} from "react-router-dom";
-import {Context} from "../store/appContext.js"
+import React, { useEffect, useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Context } from "../store/appContext.js"
+import { ContactCard } from "../component/contactCard.jsx";
 import "../../styles/home.css";
 
-export const Home = () => (
-	const {actions, store} = useContext(Context)
-	<div className="text-center mt-5">
-		
-	</div>
-);
+export const Home = () => {
+	const { actions, store } = useContext(Context)
+	const { contacts } = store
+
+	return (
+		<>
+			< div className="container" >
+				<Link to="/addContact">
+					<button className="btn btn-primary m-3">Agrega un nuevo contacto</button>
+				</Link>
+			</div >
+
+			{contacts.map((item, index) => {
+
+				return (
+					<>
+						<ContactCard key={index} contact={item} />
+					</>
+				)
+			})}
+
+		</>
+	)
+}
